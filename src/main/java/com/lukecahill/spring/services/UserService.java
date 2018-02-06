bc73fca949cf6a460a2b247bdec32603ec4b1d27
Luke Cahill <lmc@outlook.com>
2018-02-06 15:22:04 +0000

    Replace autowired annotation with inject

--- src/main/java/com/lukecahill/spring/services/UserService.java
+++ src/main/java/com/lukecahill/spring/services/UserService.java
@@ -2,23 +2,23 @@ package com.lukecahill.spring.services;
 
 import com.lukecahill.spring.models.User;
 import com.lukecahill.spring.repositories.UserRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.transaction.Transactional;
 
 @Service("userDetailsService")
 public class UserService implements UserDetailsService {
 
-    @Autowired
+    @Inject
     private UserRepository _userRepository;
 
-    @Autowired
+    @Inject
     private EntityManager entityManager;
 
     @Transactional
