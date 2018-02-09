1be670c462bd72f4dd01f2ef1eba784804c0338f
Luke Cahill <lmc@outlook.com>
2018-02-09 14:02:38 +0000

    Roles controller now using service

--- src/main/java/com/lukecahill/spring/controllers/RolesController.java
+++ src/main/java/com/lukecahill/spring/controllers/RolesController.java
@@ -1,7 +1,9 @@
 package com.lukecahill.spring.controllers;
 
 import com.lukecahill.spring.bindingmodels.RolesBindingModels;
+import com.lukecahill.spring.models.Roles;
 import com.lukecahill.spring.repositories.RolesRepository;
+import com.lukecahill.spring.services.RolesService;
 import com.lukecahill.spring.viewmodels.RolesViewModel;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -16,27 +18,27 @@ import java.util.stream.Collectors;
 @RequestMapping(path = "/api/Roles")
 public class RolesController {
 
-    private RolesRepository rolesRepository;
+    private RolesService rolesService;
 
     @Inject
-    public RolesController(RolesRepository rolesRepository) {
-        this.rolesRepository = rolesRepository;
+    public RolesController(RolesService rolesService) {
+        this.rolesService = rolesService;
     }
 
     @RequestMapping(method = RequestMethod.GET, value = "/{roleId}")
     public ResponseEntity<?> get(@PathVariable int roleId) {
-        return ResponseEntity.ok(new RolesViewModel(rolesRepository.findOne(roleId)));
+        return ResponseEntity.ok(new RolesViewModel(rolesService.get(roleId)));
     }
 
     @RequestMapping(method = RequestMethod.GET)
     public ResponseEntity<?> getAll() {
-        List<RolesViewModel> roles = rolesRepository.findAll().stream()
+        List<RolesViewModel> roles = rolesService.getAll().stream()
                 .map(x -> new RolesViewModel(x)).collect(Collectors.toCollection(ArrayList::new));
         return ResponseEntity.ok(roles);
     }
 
     @RequestMapping(method = RequestMethod.POST)
     public ResponseEntity<?> add(@RequestBody RolesBindingModels role) {
-        return ResponseEntity.ok(null);
+        return ResponseEntity.ok(rolesService.add(role));
     }
 }
