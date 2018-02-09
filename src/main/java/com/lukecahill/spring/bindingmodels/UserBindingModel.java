b2b7841c1bd73332669ca63b27396e3916f36ae8
Luke Cahill <lmc@outlook.com>
2018-02-09 10:02:00 +0000

    Update binding models to create and update classes

--- src/main/java/com/lukecahill/spring/bindingmodels/UserBindingModel.java
+++ src/main/java/com/lukecahill/spring/bindingmodels/UserBindingModel.java
@@ -1,29 +1,22 @@
 package com.lukecahill.spring.bindingmodels;
 
-import com.lukecahill.spring.models.User;
-
 public class UserBindingModel {
-	public String username;
-	public String name;
-	public String password;
-	public String email;
-	public boolean enabled;
 
-	public UserBindingModel() {}
+	public static class Create {
 
-	public UserBindingModel(User user) {
-		this.username = user.getUsername();
-		this.password = user.getPassword();
-		this.name = user.getName();
-		this.email = user.getEmail();
-		this.enabled = user.isEnabled();
+		public String username;
+		public String name;
+		public String password;
+		public String email;
+		public boolean enabled;
 	}
 
-	public UserBindingModel(String username, String name, String password, String email, boolean enabled) {
-		this.username = username;
-		this.name = name;
-		this.email = email;
-		this.password = password;
-		this.enabled = enabled;
+	public static class Update {
+
+		public String username;
+		public String name;
+		public String password;
+		public String email;
+		public boolean enabled;
 	}
 }
