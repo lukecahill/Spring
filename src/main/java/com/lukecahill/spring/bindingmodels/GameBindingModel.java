97778347e26c4d9b869353c678a3274f20fc314a
Luke Cahill <lmc@outlook.com>
2018-02-14 08:19:21 +0000

    Add game binding model

--- src/main/java/com/lukecahill/spring/bindingmodels/GameBindingModel.java
+++ src/main/java/com/lukecahill/spring/bindingmodels/GameBindingModel.java
@@ -1,4 +1,44 @@
 package com.lukecahill.spring.bindingmodels;
 
+import javax.validation.constraints.NotNull;
+
 public class GameBindingModel {
+
+    public static class Create {
+        @NotNull
+        public String gameName;
+        @NotNull
+        public String gamePublisher;
+        @NotNull
+        public double gamePrice;
+
+        public Create() {}
+
+        public Create(String gameName, String gamePublisher, double gamePrice) {
+            this.gameName = gameName;
+            this.gamePublisher = gamePublisher;
+            this.gamePrice = gamePrice;
+        }
+    }
+
+    public static class Update {
+
+        @NotNull
+        public String gameName;
+        @NotNull
+        public String gamePublisher;
+        @NotNull
+        public double gamePrice;
+        @NotNull
+        public int gameId;
+
+        public Update() {}
+
+        public Update(String gameName, String gamePublisher, double gamePrice, int gameId) {
+            this.gameName = gameName;
+            this.gamePublisher = gamePublisher;
+            this.gamePrice = gamePrice;
+            this.gameId = gameId;
+        }
+    }
 }
