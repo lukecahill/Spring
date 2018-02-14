37736a378db38568d26ae48ea415a455d33948cc
Luke Cahill <lmc@outlook.com>
2018-02-14 14:03:28 +0000

    Edit how the initial data is run - using config variable

--- src/main/java/com/lukecahill/spring/config/DataLoader.java
+++ src/main/java/com/lukecahill/spring/config/DataLoader.java
@@ -6,6 +6,7 @@ import com.lukecahill.spring.models.UserRoles;
 import com.lukecahill.spring.repositories.GameRepository;
 import com.lukecahill.spring.repositories.RolesRepository;
 import com.lukecahill.spring.repositories.UserRolesRepository;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 
@@ -23,6 +24,9 @@ public class DataLoader implements ApplicationRunner {
     private UserRolesRepository userRolesRepository;
     private GameRepository gameRepository;
 
+    @Value("${sql.initial.run}")
+    private String initialRun;
+
     public DataLoader() {
     }
 
@@ -37,20 +41,23 @@ public class DataLoader implements ApplicationRunner {
     @Override
     public void run(ApplicationArguments args) throws Exception {
 
-        // creates a user with the username 'luke' and password 'password'
-        // also creates roles, and assigns luke the admin role.
-        User user = new User("luke", "luke", "lmc@outlook.com",
-                "$2a$04$hW1MnBIdgNI/4FCW/33h7eB3RNJ5OkDP6VgVm2p6Oi1VcyfVmr2nu", true);
-        userRepository.save(user);
+        if("true".equals(initialRun)) {
 
-        rolesRepository.save(new Roles(1, "READ_WRITE"));
-        rolesRepository.save(new Roles(2, "READ"));
-        Roles adminRole = new Roles(3, "ADMIN");
-        rolesRepository.save(adminRole);
+            // creates a user with the username 'luke' and password 'password'
+            // also creates roles, and assigns luke the admin role.
+            User user = new User("luke", "luke", "lmc@outlook.com",
+                    "$2a$04$hW1MnBIdgNI/4FCW/33h7eB3RNJ5OkDP6VgVm2p6Oi1VcyfVmr2nu", true);
+            userRepository.save(user);
 
-        userRolesRepository.save(new UserRoles(1, user, adminRole));
+            rolesRepository.save(new Roles(1, "READ_WRITE"));
+            rolesRepository.save(new Roles(2, "READ"));
+            Roles adminRole = new Roles(3, "ADMIN");
+            rolesRepository.save(adminRole);
 
-        gameRepository.save(new Game(1, "Rainbow Six: Siege", "Ubisoft", 12.99D));
-        gameRepository.save(new Game(2, "Total War: Britannia", "Sega", 39.99D));
+            userRolesRepository.save(new UserRoles(1, user, adminRole));
+
+            gameRepository.save(new Game(1, "Rainbow Six: Siege", "Ubisoft", 12.99D));
+            gameRepository.save(new Game(2, "Total War: Britannia", "Sega", 39.99D));
+        }
     }
 }
