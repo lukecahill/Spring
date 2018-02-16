fb07a33ad3b66cf7e2c1b6f3f2115859018be481
Luke Cahill <lmc@outlook.com>
2018-02-16 11:37:09 +0000

    Rename tests to integration test

--- src/test/java/com/lukecahill/spring/tests/IntegrationTests.java
+++ src/test/java/com/lukecahill/spring/tests/IntegrationTests.java
@@ -1,13 +1,18 @@
 package com.lukecahill.spring.tests;
 
+import com.lukecahill.spring.bindingmodels.GameBindingModel;
 import com.lukecahill.spring.bindingmodels.UserBindingModel;
 import com.lukecahill.spring.config.Application;
+import com.lukecahill.spring.models.Game;
 import com.lukecahill.spring.models.Roles;
 import com.lukecahill.spring.models.User;
 import com.lukecahill.spring.repositories.RolesRepository;
+import com.lukecahill.spring.services.GameService;
 import com.lukecahill.spring.services.UserService;
+import com.lukecahill.spring.viewmodels.GameViewModel;
 import org.junit.*;
 import org.junit.runner.RunWith;
+import org.junit.runners.MethodSorters;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -16,10 +21,17 @@ import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.inject.Inject;
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+import java.util.List;
+import java.util.Set;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = {Application.class})
 @TestPropertySource(locations = "classpath:application-integrationtest.properties")
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 public class Tests {
 
     @Inject
@@ -28,8 +40,19 @@ public class Tests {
     @Inject
     private RolesRepository rolesRepository;
 
+    @Inject
+    private GameService gameService;
+
     private User testuser = new User("testuser", "testuser", "testuser@example.com",
             "", false);
+    private Game testGame = new Game(10, "Rome: Total War", "Sega", 3.99D);
+    private static Validator validator;
+
+    @Before
+    public void setUp() {
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        validator = factory.getValidator();
+    }
 
     @Test
     public void ifCreateNewUser_shouldCreateUser() {
@@ -57,8 +80,9 @@ public class Tests {
 
     @Test
     public void ifRolesFindOneIsValid_shouldReturnRole() {
+        List<Roles> roles = rolesRepository.findAll();
         Roles role = rolesRepository.findOne(1);
-        Roles mockRole = new Roles(1, "READ_WRITE");
+        Roles mockRole = new Roles(1, "TEST_ROLE");
         Assert.assertEquals(mockRole.getRoleName(), role.getRoleName());
     }
 
@@ -106,12 +130,45 @@ public class Tests {
         Assert.assertEquals(null, updated); // this will throw
     }
 
+    @Test
+    public void ifGameAddIsValid_gameShouldBeAddedToDatabase() {
+        GameBindingModel.Create gameToAdd = new GameBindingModel.Create( "Rome: Total War", "Sega", 3.99D);
+        Game addedGame = gameService.add(gameToAdd);
+
+        Assert.assertEquals(gameToAdd.gameName, addedGame.getName());
+        Assert.assertEquals(gameToAdd.gamePrice, addedGame.getPrice(), 0.1D);
+        Assert.assertEquals(gameToAdd.gamePublisher, addedGame.getPublisher());
+    }
+
+    @Test
+    public void ifGameGetIsValid_gameShouldBeRetrievedFromDatabase() {
+        Game retrievedGame = gameService.get(1);
+        Assert.assertEquals(testGame.getName(), retrievedGame.getName());
+    }
+
     @Test(expected = NullPointerException.class)
-    public void ifUserPasswordUpdateUsernameIsNotValid_userPasswordShouldReturnNull() {
-        UserBindingModel.Update updatedUser = new UserBindingModel.Update();
-        updatedUser.username = "testuser";
-        updatedUser.password = "$2a$04$.9KuvEVK06bEZgz5d1YeNOIQYNRqJ3CnMjAw8UeA8Xl79K8R4SwWS";
-        User updated = userService.updatePassword("testuser", updatedUser);
-        Assert.assertEquals(null, updated); // this will throw
+    public void ifGameGetIsInvalid_shouldThrowNull() {
+        Game retrievedGame = gameService.get(0);
+        Assert.assertEquals(testGame.getName(), retrievedGame.getName());
+    }
+
+    @Test
+    public void ifGameUpdateIsValid_gameShouldBeEditedInDatabase() {
+        GameBindingModel.Update gameToUpdate = new GameBindingModel.Update();
+        gameToUpdate.gamePublisher = "test";
+        gameToUpdate.gameName = "test";
+        gameToUpdate.gamePrice = 45D;
+
+        Game updated = gameService.update(1, gameToUpdate);
+        Assert.assertEquals(gameToUpdate.gameName, updated.getName());
+        Assert.assertEquals(gameToUpdate.gamePublisher, updated.getPublisher());
+        Assert.assertEquals(gameToUpdate.gamePrice, updated.getPrice(), 0.1D);
+    }
+
+    @Test
+    public void ifGameAddIsInvalid_gameValidationShouldFail() {
+        Game gameToFail = new Game(0, "", "", 10.0D);
+        Set<ConstraintViolation<Game>> violations = validator.validate(gameToFail);
+        Assert.assertFalse(violations.isEmpty());
     }
 }
